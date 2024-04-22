e4d1875755ec1658a49b5447268558c24e9a85c0
Samiya142 <samiya2011099@gmail.com>
2024-04-22 13:58:36 +0500

    Developing a todo list by using typescript, inquirer and node.js.

---
+++ main.ts
@@ -0,0 +1,34 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todoList = []
+
+console.log(chalk.blue.bold("\n \t Welcome to todo list Aplication by Samiya.\n") );
+
+let condition = true;
+
+while(condition){
+    let addTask =await inquirer.prompt([
+        {
+            name: "Task",
+            type: "input",
+            message: "Please enter a task in your todo list"
+        },
+   ]) 
+   todoList.push(addTask.Task);
+   console.log(`"${addTask.Task}" added to todo list successfully!`);
+
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "moreTask",
+            type: "confirm",
+            message: "Do you want to add more task?",
+            default: "True"
+        }
+    ])
+    condition = addMoreTask.moreTask
+}
+console.log("Your updated todo list:", todoList);
+
